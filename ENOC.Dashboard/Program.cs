62d72e6a2a5cbe091b06a273af29a0a7870a7805
Tahar <tahar@kuality.ai>
2025-09-30 00:48:28 +0400

    Aligned with new UI

--- ENOC.Dashboard/Program.cs
+++ ENOC.Dashboard/Program.cs
@@ -2,8 +2,6 @@ using ENOC.Dashboard.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ENOC.Dashboard.Data.DataHelper.Initialize(builder.Environment);
-
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
